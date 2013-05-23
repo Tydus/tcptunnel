9b56575ae35eaece79f46271a9b55931df41985a
Tydus <Tydus@Tydus.org>
2013-05-23 11:51:00 +0800

    Fix in b64.h

--- b64.h
+++ b64.h
@@ -1,8 +1,10 @@
 #ifndef _B64_H
 #define _B64_H
 
-int b64_encode(const char *in, int in_len, char *out, int out_size);
+#include <stdint.h>
 
-int b64_decode(const char *in, char *out, int out_size);
+int b64_encode(const uint8_t *in, int in_len, char *out, int out_size);
+
+int b64_decode(const char *in, uint8_t *out, int out_size);
 
 #endif
